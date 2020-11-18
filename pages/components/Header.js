1c1bce1e9dd753ae7cb12978b5e3bb961dd2c7c8
OnceLearner <redaamir972@gmail.com>
2020-11-18 04:33:50 +0100

    adding menue icon

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from "next/image"
 import Link from "next/link"
 import tw from 'twin.macro'
+import MenuIcon from '@material-ui/icons/Menu'
 
 const tabs = [{ id: 1, href: "/", name: "Home" }, { id: 2, href: "/About", name: "About" }, { id: 3, href: "/ReachMe", name: "Reach Me" }]
 
 const Header = () => {
+    const [screenSize, setScreenSize] = useState(process.browser ? window.innerWidth : null);
+    if (process.browser) window.onresize = () => { setScreenSize(window.innerWidth) }
+
+
+
+
     return (
-        <div css={tw`flex justify-evenly     `}>
+        <div css={tw`flex justify-evenly  md:mb-1 mb-12     `}  >
             <Link href="/">
                 <Image css={tw` hover:cursor-pointer object-cover`} src="/logo.png" height="100" width="150vw" />
             </Link>
-
-            <ul css={tw` flex  justify-evenly gap-5 mt-8`} >
-                {tabs.map(tab => (
-                    <li key={tab.id} >
-                        <Link href={tab.href}>
-                            <a css={tw` hover:border-b-4 text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-gray-700 to-gray-900  hover:border-indigo-400 hover:text-indigo-500 hover:cursor-pointer p-2`}>{tab.name}</a>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-
+            {screenSize > 700 ?
+                <ul css={tw` flex  justify-evenly gap-5 mt-8`} >
+                    {tabs.map(tab => (
+                        <li key={tab.id} >
+                            <Link href={tab.href}>
+                                <a css={tw` hover:border-b-4 text-lg font-semibold bg-clip-text text-transparent bg-gradient-to-r from-gray-700 to-gray-900  hover:border-indigo-400 hover:text-indigo-500 hover:cursor-pointer p-2`}>{tab.name}</a>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                : <MenuIcon css={tw` mt-4 ml-24  `} style={{ fontSize: "40px" }} />
+            }
 
         </div >
     )
--- pages/components/TemplateOption.js
+++ pages/components/TemplateOption.js
@@ -3,12 +3,14 @@ import Link from 'next/link'
 import React from 'react'
 import tw from 'twin.macro'
 import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 
 
 
 const TemplateOption = () => {
     return (
         <Link href="/template">
+
             <a css={tw` md:w-5/12    p-8 flex   flex-col items-center   shadow-lg hover:shadow-2xl transform  transition ease-in duration-300 hover:border-2 hover:border-pink-500    hover:-translate-y-2  h-64 hover:cursor-pointer bg-gradient-to-r from-pink-500 to-red-500`}>
                 <div css={tw`text-center text-5xl font-extrabold leading-none mb-2 `}>
                     <span style={{ fontFamily: "montserrat" }} css={tw`bg-clip-text text-transparent bg-gradient-to-r from-orange-800 to-black `}>
@@ -16,7 +18,7 @@ const TemplateOption = () => {
                      </span>
 
                 </div>
-                {/* <LibraryBooksIcon css={tw`text-5xl  text-gray-900`} /> */}
+
 
                 <div css={tw`flex justify-evenly items-center p-3`}>
                     <p css={tw` text-center  text-sm text-gray-800 font-semibold mr-2 `} > use our  collections of ready-to-use templates </p>
