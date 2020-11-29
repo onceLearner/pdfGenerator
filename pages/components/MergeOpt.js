fdb80bec3f556fcc5b75bf976738a9ee31631bb4
OnceLearner <redaamir972@gmail.com>
2020-11-29 02:36:51 +0100

    adding new cardl

--- pages/components/MergeOpt.js
+++ pages/components/MergeOpt.js
@@ -10,14 +10,14 @@ const MergeOpt = (props) => {
 
     return (
         <Link href="/pdfLib">
-            <a css={tw` flex flex-col items-center p-8 md:w-5/12 w-full shadow-lg hover:border-2 hover:border-purple-500   h-64 hover:shadow-2xl transition ease-in duration-200   transform  hover:-translate-y-2 hover:cursor-pointer  bg-gradient-to-r  from-blue-200 to-pink-300`}>
+            <a css={tw` flex flex-col items-center p-8 md:w-5/12 w-full shadow-lg hover:border-2 hover:border-gray-600   h-64 hover:shadow-2xl transition ease-in duration-200   transform  hover:-translate-y-2 hover:cursor-pointer  bg-gradient-to-r  from-gray-700 to-gray-800`}>
                 <div css={tw`text-center text-5xl font-extrabold leading-none mb-8 `}>
-                    <span style={{ fontFamily: "montserrat" }} css={tw`bg-clip-text text-transparent bg-gradient-to-r from-gray-800 to-gray-500 `}>
+                    <span style={{ fontFamily: "montserrat" }} css={tw`bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-gray-200 `}>
                         Merge two Pdfs
                      </span>
                 </div>
                 <div css={tw`flex justify-between items-center p-2`}>
-                    <p css={tw` text-center  text-sm text-gray-600 font-semibold  px-2`} > create custom pdf  using your files </p>
+                    <p css={tw` text-center  text-sm text-gray-600 font-semibold  px-2`} > produce one pdf using two different pdfs </p>
                     <ArrowRightAltIcon css={tw` text-purple-300`} />
                 </div>
 
