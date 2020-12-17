c1fd85bc8fd1628b7b26d8f4fa6872d97bcb5646
OnceLearner <redaamir972@gmail.com>
2020-12-17 16:06:47 +0100

    test-api 2

--- pages/api/hamid.js
+++ pages/api/hamid.js
@@ -9,6 +9,12 @@ export default (req, res) => {
     var erroris = "";
 
 
+    const dirRelativeToPublicFolder = 'asset'
+
+    const dir = path.resolve('./public', dirRelativeToPublicFolder);
+
+    const filenames = fs.readdirSync(dir);
+
     try {
         // get the actual path of the project
         var path1 = process.cwd();
@@ -20,5 +26,5 @@ export default (req, res) => {
 
 
     res.statusCode = 200;
-    res.send({ pathIs: path1 })
+    res.send({ pathIs: filenames, erro: erroris })
 }
