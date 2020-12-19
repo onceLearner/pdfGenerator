72bfe6ed1599c2f5988a3ed1285359c1baa5b623
OnceLearner <redaamir972@gmail.com>
2020-12-19 05:38:06 +0100

    add: header coloring depending on page

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -7,18 +7,35 @@ import HeaderPopUp from './HeaderPopUp'
 
 const tabs = [{ id: 1, href: "/", name: "Home" }, { id: 2, href: "/About", name: "About" }, { id: 3, href: "/ReachMe", name: "Reach Me" }]
 
+// check to see if we are in the landing page
+const checkIsHome = () => {
+    if (!process.browser) return true;
+    switch (window.location.href) {
+        case "http://localhost:3000/":
+            return true;
+
+        case "https://pdf-generator-steel.vercel.app/":
+            return true;
+
+        default:
+            return false;
+
+    }
+}
+
 const Header = () => {
+    const [isHome, setIsHome] = useState(checkIsHome())
     const [screenSize, setScreenSize] = useState(process.browser ? window.innerWidth : null);
     if (process.browser) window.onresize = () => { setScreenSize(window.innerWidth) }
 
 
 
     return (
-        <div css={tw`flex justify-between items-center   p-4  bg-gradient-to-b from-purple-500 to-purple-500   border border-b border-purple-400    `}  >
+        <div css={[tw`flex justify-between items-center   p-4  bg-gradient-to-b   border border-b  px-8   `, isHome ? tw`from-purple-500 to-purple-500  border-purple-400 ` : tw`from-white to-white`]}  >
             <Link href="/">
                 <div css={tw` flex items-center cursor-pointer `} >
                     <Image src="/logo-white.png" width="40" height="30" css={tw`  object-contain`} />
-                    <a css={tw` md:text-xl text-xs text-white`} style={{ fontFamily: "Audiowide" }} >Publisher Factory</a>
+                    <a css={[tw` md:text-xl text-xs text-white`, isHome ? tw`text-white` : tw`text-gray-800`]} style={{ fontFamily: "Audiowide" }} >Publisher Factory</a>
                 </div>
 
 
@@ -31,7 +48,7 @@ const Header = () => {
                         {tabs.map(tab => (
                             <li key={tab.id} >
                                 <Link href={tab.href}>
-                                    <a style={{ fontFamily: "Questrial" }} css={tw` hover:border-b-4 text-gray-100 text-lg   font-thin   hover:border-indigo-400 hover:text-indigo-500 hover:cursor-pointer p-2`}>{tab.name}</a>
+                                    <a style={{ fontFamily: "Questrial" }} css={[tw` hover:border-b-4  text-lg   font-thin   hover:border-indigo-900 hover:text-indigo-900 hover:cursor-pointer p-2`, isHome ? tw`text-gray-100` : tw`text-gray-700`]}>{tab.name}</a>
                                 </Link>
                             </li>
                         ))}
